064aaeb13c3c916f811fb6952f1392ac75cbb396
Arjun Kula <skvarjun@gmail.com>
2025-12-16 00:01:47 +0000

    Added few blades for drone data visualization

---
+++ resources/views/frontend/pages/drone.blade.php
@@ -0,0 +1,26 @@
+@extends('frontend.layouts.app')
+
+@section('title', __('Drone Data Test Results'))
+
+@section('content')
+    <div class="container py-4">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <x-frontend.card>
+                    <x-slot name="header">
+                        Drone Test Data Visualizations
+                    </x-slot>
+
+                    <x-slot name="body">
+                        <iframe 
+                            src="{{ asset('assets/drone_test_data/drone_map.html') }}" 
+                            width="100%" 
+                            height="600" 
+                            style="border:none;">
+                        </iframe>
+                    </x-slot>
+                </x-frontend.card>
+            </div><!--col-md-10-->
+        </div><!--row-->
+    </div><!--container-->
+@endsection
